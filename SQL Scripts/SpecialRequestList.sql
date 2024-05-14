9d9c632620a72062386ec9722f8869d964738e38
Daren <darencantrell@gmail.com>
2024-05-14 15:57:43 +0100

    Data Produced

--- SQL Scripts/SpecialRequestList.sql
+++ SQL Scripts/SpecialRequestList.sql
@@ -1,5 +1,5 @@
 
-Declare @Show_ID uniqueidentifier = '5BDD261C-B420-4830-9646-CF8C8365B1C6'
+Declare @Show_ID uniqueidentifier = '86BFB3B8-DF78-4DF1-B12E-0AEF81668F88'
 --Declare @Show_Entry_Class_ID uniqueidentifier = '9613FDC2-897C-45E1-B80F-F2294C797AB7'
 Declare @Show_Entry_Class_ID uniqueidentifier = null
 Declare @SpecialRequestsOnly bit = 1
@@ -14,38 +14,38 @@ select
 	,dc.Dog_Class_ID
 	,sfc.Show_Entry_Class_ID
 	,sfc.Show_Final_Class_ID
-FROM [dsm].[dbo].[tblDog_Classes] dc
-inner join [dsm].[dbo].[tblDogs] d
+FROM [Petersfield_2024].[dbo].[tblDog_Classes] dc
+inner join [Petersfield_2024].[dbo].[tblDogs] d
 on dc.Dog_ID = d.Dog_ID
 and d.Deleted_By is null
 and ((@SpecialRequestsOnly = 1 AND nullif(dc.Special_Request,'') is not null)
 or (@SpecialRequestsOnly = 0 AND nullif(dc.Special_Request,'') is null))
-inner join [dsm].[dbo].[lnkDog_Owners] do
+inner join [Petersfield_2024].[dbo].[lnkDog_Owners] do
 on d.Dog_ID = do.Dog_ID
 and do.Deleted_By is null
-inner join [dsm].[dbo].[tblPeople] owners
+inner join [Petersfield_2024].[dbo].[tblPeople] owners
 on do.Owner_ID = owners.Person_ID
 and owners.Deleted_By is null
-inner join [dsm].[dbo].[tblEntrants] e
+inner join [Petersfield_2024].[dbo].[tblEntrants] e
 on dc.Entrant_ID = e.Entrant_ID
 and e.Deleted_By is null
-inner join [dsm].[dbo].[tblShow_Entry_Classes] sec
+inner join [Petersfield_2024].[dbo].[tblShow_Entry_Classes] sec
 on dc.Show_Entry_Class_ID = sec.Show_Entry_Class_ID
 and sec.Deleted_By is null
 and (sec.Show_Entry_Class_ID = @Show_Entry_Class_ID OR @Show_Entry_Class_ID IS NULL)
-inner join [dsm].[dbo].[tblShows] s
+inner join [Petersfield_2024].[dbo].[tblShows] s
 on sec.Show_ID = s.Show_ID
-inner join [dsm].[dbo].[tblShow_Final_Classes] sfc
+inner join [Petersfield_2024].[dbo].[tblShow_Final_Classes] sfc
 on dc.Show_Final_Class_ID = sfc.Show_Final_Class_ID
 and s.Show_ID = @Show_ID
-inner join [dsm].[dbo].[lkpClass_Names] cn
+inner join [Petersfield_2024].[dbo].[lkpClass_Names] cn
 on sec.Class_Name_ID = cn.Class_Name_ID
 and cn.Class_Name_Description <> 'NFC'
 inner join
 (select @Show_ID as Show_ID
 Union ALL
 select Child_Show_ID as Show_ID
-from [dsm].[dbo].[lnkLinked_Shows] ls
+from [Petersfield_2024].[dbo].[lnkLinked_Shows] ls
 where Parent_Show_ID = @Show_ID) sl
 on sec.Show_ID = sl.Show_ID
 where dc.Deleted_By is null
